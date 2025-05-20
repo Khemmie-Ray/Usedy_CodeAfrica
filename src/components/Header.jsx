f9a55ea20594c04f0a9a72039197faf27763ee33
Khemmie-Ray <atokemmy@gmail.com>
2025-05-20 22:13:11 +0100

    refactor and integration setup

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,13 @@ import  { useState } from "react";
 import logo from "../assets/logo.svg";
 import { NavLink } from "react-router-dom";
 import { Sling as Hamburger } from "hamburger-react";
+import { useAppKit } from "@reown/appkit/react";
+import { useAppKitAccount } from "@reown/appkit/react";
 
 const Header = () => {
   const [isOpen, setOpen] = useState(false);
+  const { isConnected } = useAppKitAccount()
+  const { open } = useAppKit()
 
   return (
     <header className="py-8 bg-[#073F77]/5">
@@ -30,7 +34,7 @@ const Header = () => {
             About Us
           </a>
         </nav>
-        {/* {!isConnected ? <button className="bg-[#154A80] rounded-lg p-4 text-white font-montserrat mr-4 lg:text-[20px] md:text-[20px] text-[18px]" onClick={() => open()}>Connect Wallet</button> : <w3m-button />} */}
+        {!isConnected ? <button className="bg-[#154A80] rounded-lg p-3 text-white font-montserrat mr-4 text-[18px]" onClick={() => open()}>Connect Wallet</button> : <w3m-button />}
       </div>
       <div className="w-[95%] mx-auto flex justify-between lg:hidden md:hidden relative">
         <img src={logo} alt="" className="w-[185px] h-[43px]" />
@@ -55,7 +59,7 @@ const Header = () => {
             About Us
           </a>
           <div className="mt-6">
-          {/* {!isConnected ? <button className="bg-[#154A80] rounded-lg p-4 text-white font-montserrat mr-4 lg:text-[20px] md:text-[20px] text-[18px]" onClick={() => open()}>Connect Wallet</button> : <w3m-button />} */}
+          {!isConnected ? <button className="bg-[#154A80] rounded-lg p-4 text-white font-montserrat mr-4 text-[18px]" onClick={() => open()}>Connect Wallet</button> : <w3m-button />}
         </div>
         </nav>)}
       </div>
